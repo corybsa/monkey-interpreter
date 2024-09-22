c3d720e5d22580b66cdab376f83428749bb231a0
Cory Sandlin <corybsa@gmail.com>
2024-09-22 18:58:35 -0400

    create a string method on the ast for easy testing

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,13 @@
 package ast
 
 import (
+	"bytes"
 	"monkey/token"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -25,6 +27,20 @@ func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
 type ReturnStatement struct {
 	Token       token.Token // the token.RETURN token
 	ReturnValue Expression
@@ -36,13 +52,40 @@ func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
 
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
+
 type Expression interface {
 	Node
 	expressionNode()
-}
-
-type Program struct {
-	Statements []Statement
 }
 
 type Identifier struct {
@@ -50,16 +93,34 @@ type Identifier struct {
 	Value string
 }
 
-func (identifier *Identifier) expressonNode() {}
+func (i *Identifier) expressionNode() {}
 
-func (identifier *Identifier) TokenLiteral() string {
-	return identifier.Token.Literal
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
 
-func (program *Program) TokenLiteral() string {
-	if len(program.Statements) > 0 {
-		return program.Statements[0].TokenLiteral()
+func (i *Identifier) String() string {
+	return i.Value
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
 	} else {
 		return ""
 	}
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
 }
