662dacefd4c2622ad2b9dec20d6811534e971a36
Cory Sandlin <sandlincb@upmc.edu>
2024-10-03 09:30:50 -0400

    change format

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -37,9 +37,9 @@ func (l *Lexer) NextToken() token.Token {
 	switch l.ch {
 	case '=':
 		if l.peekChar() == '=' {
-			var ch = l.ch
+			ch := l.ch
 			l.readChar()
-			var literal = string(ch) + string(l.ch)
+			literal := string(ch) + string(l.ch)
 			tok = token.Token{Type: token.EQ, Literal: literal}
 		} else {
 			tok = newToken(token.ASSIGN, l.ch)
@@ -50,9 +50,9 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.MINUS, l.ch)
 	case '!':
 		if l.peekChar() == '=' {
-			var ch = l.ch
+			ch := l.ch
 			l.readChar()
-			var literal = string(ch) + string(l.ch)
+			literal := string(ch) + string(l.ch)
 			tok = token.Token{Type: token.NOT_EQ, Literal: literal}
 		} else {
 			tok = newToken(token.BANG, l.ch)
